bbfbf79dc835f0eaed809f085497841c378ac6aa
Ryan Arana <ryan@ryanarana.com>
2014-08-17 14:07:20 -0700

    initial commit, it builds

---
+++ expirement-monogame/expirement-monogame/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+using experiment.monogame;
+
+namespace expirementmonogame
+{
+	public partial class AppDelegate : NSApplicationDelegate
+	{
+		Game1 game;
+		public AppDelegate ()
+		{
+		}
+
+		public override void FinishedLaunching(MonoMac.Foundation.NSObject notification) {
+			game = new Game1 ();
+			game.Run ();
+		}
+
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender) {
+			return true;
+		}
+	}
+}
+
---
+++ expirement-monogame/expirement-monogame/Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+using experiment.monogame;
+
+namespace expirement.monogame {
+	static class Program {
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+
+			using (var p = new NSAutoreleasePool ()) {
+				NSApplication.SharedApplication.Delegate = new AppDelegate ();
+				NSApplication.Main (args);
+			}
+		}
+	}
+
+	class AppDelegate : NSApplicationDelegate {
+	}
+}
+namespace experiment.monogame {
+
+	public class Game1 : Game {
+		GraphicsDeviceManager graphics;
+
+		public Game1() {
+			graphics = new GraphicsDeviceManager (this);
+			Content.RootDirectory = "Content";
+			graphics.IsFullScreen = false;
+		}
+
+		protected override void Initialize ()
+		{
+			base.Initialize ();
+		}
+
+		protected override void Update(GameTime gameTime) {
+			base.Update (gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime) {
+			graphics.GraphicsDevice.Clear (Color.DarkRed);
+
+			base.Draw (gameTime);
+		}
+	}
+}
+
